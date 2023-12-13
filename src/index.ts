8e8ed82fbbe0e03f035e05adb65c62904ee87b85
Casper Tollefsen <casper.tollefsen@telenor.no>
2023-12-13 13:06:34 +0100

    Add getTheme function to retrieve logger options from a theme

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
+import { ThemeName } from './_generated/theme'
+import { LoggerOptions } from './types'
+
 export { default as Logger } from './classes/Logger'
+
+/**
+ * Gets the logger options from a theme.
+ * @param theme The theme name.
+ * @returns The logger options.
+ */
+export function getTheme(theme: ThemeName): LoggerOptions {
+  const options = require(`./themes/${theme}`).default
+
+  if (!options) {
+    throw new Error(`Theme "${theme}" does not contain a valid configuration.`)
+  }
+
+  return options
+}
